22828e5b62d8e5176f85c9f0a6cb3b0ade156b71
Marco Fernandes <marcoaferc@gmail.com>
2021-07-05 20:00:08 -0300

    Adicionar interfaces de service e repo

---
+++ lib/app/core/repositories/interface_storage_repository.dart
@@ -0,0 +1,8 @@
+import 'package:estacionamento_joao/app/core/models/vehicle_entry_model.dart';
+
+abstract class InterfaceStorageRepository{
+  Future<List<VehicleEntryModel>> getAllEntries();
+  Future<List<VehicleEntryModel>> getActiveEntries();
+  Future<bool> insertEntry(VehicleEntryModel model);
+  Future<bool> updateEntry(VehicleEntryModel model);
+}
