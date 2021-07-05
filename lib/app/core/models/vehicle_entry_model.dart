4f566a4dbe7a0bbec7a67841f298a6b59d138428
Marco Fernandes <marcoaferc@gmail.com>
2021-07-05 19:42:23 -0300

    Criar modelo de registro de veiculos

---
+++ lib/app/core/models/vehicle_entry_model.dart
@@ -0,0 +1,33 @@
+class VehicleEntryModel{
+  final int slotId;
+  final DateTime start;
+  final DateTime? end;
+  final int? id;
+
+  VehicleEntryModel(
+    {
+      required this.slotId, 
+      required this.start,
+      this.end, 
+      this.id
+    }
+  );
+
+  factory VehicleEntryModel.fromJson(Map<String, dynamic> json){
+    return VehicleEntryModel(
+      slotId: json['slotId'], 
+      start: DateTime.fromMillisecondsSinceEpoch(json['start']),
+      end: DateTime.fromMillisecondsSinceEpoch(json['end']),
+      id: json['id']
+    );
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      'slotId': slotId, 
+      'start': start,
+      'end': end,
+      'id': id
+    };
+  }
+}
